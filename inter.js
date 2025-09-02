3627ad9a4fb2835227deee4645c5d7a4bcba2ee5
Aseel khanfar <57755284+Aseelkhanfar19@users.noreply.github.com>
2025-09-03 01:38:10 +0300

    finish the game
    - add components
    - add style
    - add interaction by js

--- inter.js
+++ inter.js
@@ -3,42 +3,124 @@ var rangeValues; // range of values
 var guessedNumber; //player will guess the number
 var attempts=5;  // the player have 5 attempts
 var startGame = false; //did the game running or not
+var searching=true; 
 
 function main(){
     //will call all functions here
+    startTheGame();
 
 }
 
 function startTheGame()
 {
     //to start the game
+    setTimeout(searchingForNumber,5000);
+    
 
 }
 
 function searchingForNumber(){
-    //computer will chose a randome number
+    
+    choosenNumber=Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+    // alert(choosenNumber);
+    dispalyTheTextBox();
+
 
 }
+function dispalyTheTextBox(){
+    document.getElementById("progeress").innerHTML="Guess The Number Between 1-20";
+    document.getElementById("contGuessBox").style.display="block";
+    hideLoading();
+}
+
+function hideTheTextBox(){
+    document.getElementById("contGuessBox").style.display="none";
+
+}
+
+function displayLoading(){
+    document.getElementById("progeress").innerHTML="Choosing number";
+    document.getElementById("boxChanging").style.display="inline";
+    hideTheTextBox();
+
+    
+
+}
+
+function hideLoading(){
+    document.getElementById("boxChanging").style.display="none";
+
+}
+
+
 
 function guessTheNumber()
 {
-
+  guessedNumber=parseInt(document.getElementById("guessedNumBox").value);
 }
 
 
-function checkTheGuessedNum(){
+function checkTheGuessedNum(event){
+    if(event.key=="Enter"){
     //the computer will check the quessed number by user and decide if it too high of too low
+    guessTheNumber();
+    if(guessedNumber==choosenNumber)
+    {
+        document.getElementById("numLbl").innerHTML="Correct!!";
+        setTimeout(function(){
+        stopTheGame()
+        startTheGame();},700);
+    }
+    else if(guessedNumber>=choosenNumber+2 && guessedNumber<choosenNumber+5)
+    {
+        document.getElementById("numLbl").innerHTML="Too close";
+        countTheAttempts();
+    }
+    else if(guessedNumber >= choosenNumber+5)
+    {
+         document.getElementById("numLbl").innerHTML="Too high";
+         countTheAttempts();
+    }
+    else if(guessedNumber>=choosenNumber-2 && guessedNumber>choosenNumber-5 )
+    {
+        document.getElementById("numLbl").innerHTML="Too close";
+        countTheAttempts();
+    }
+    else if(guessedNumber<=choosenNumber-5)
+    {
+      document.getElementById("numLbl").innerHTML="Too low";  
+      countTheAttempts();
+    }
 
+    }
 }
 
 function countTheAttempts()
 {
-    //will count the attempts and return the remaining attempts 
+    attempts--;
+    document.getElementById("attmptLbl").innerHTML=attempts; 
+    setTimeout(function(){
+    if(attempts==0){
+    alert("Game Over!");
+    stopTheGame();
+    }
+    else{
+        
+
+    }
+    },1000);
 }
 
 function stopTheGame(){
 
     //stop the game
+    attempts=5;
+    document.getElementById("attmptLbl").innerHTML=attempts;
+    document.getElementById("numLbl").innerHTML="";  
+    document.getElementById("guessedNumBox").value="";
+    displayLoading();
+    startTheGame();
+
 
 }
 
