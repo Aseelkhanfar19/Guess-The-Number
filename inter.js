078746b10f6d1eb53205a544091ee977b39011e5
Aseel khanfar <57755284+Aseelkhanfar19@users.noreply.github.com>
2025-09-02 20:37:26 +0300

    add functions and variables

---
+++ inter.js
@@ -0,0 +1,47 @@
+var choosenNumber; //the computer will choose the number
+var rangeValues; // range of values
+var guessedNumber; //player will guess the number
+var attempts=5;  // the player have 5 attempts
+var startGame = false; //did the game running or not
+
+function main(){
+    //will call all functions here
+
+}
+
+function startTheGame()
+{
+    //to start the game
+
+}
+
+function searchingForNumber(){
+    //computer will chose a randome number
+
+}
+
+function guessTheNumber()
+{
+
+}
+
+
+function checkTheGuessedNum(){
+    //the computer will check the quessed number by user and decide if it too high of too low
+
+}
+
+function countTheAttempts()
+{
+    //will count the attempts and return the remaining attempts 
+}
+
+function stopTheGame(){
+
+    //stop the game
+
+}
+
+
+
+
